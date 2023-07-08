0116cf947c7ea905c46c002249f92dd6ec96770c
Yangsta911 <yangxin.sz.ch@gmail.com>
2023-07-08 20:52:05 +1000

    attempted to add multi threading

--- echo_client.py
+++ echo_client.py
@@ -2,24 +2,64 @@ import socket
 import sys
 import json
 import time
+import threading
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def alive_check():
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
-sock.connect(server_address)
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 10000)
+    print >>sys.stderr, 'connecting to %s port %s\n' % server_address
+    sock.connect(server_address)
 
 
-send_data = '{"id": 8000}'
-data = json.loads(send_data)
+    data_alive = '{"id": 8000}'
+    send_alive = json.loads(data_alive)
 
-while True:
-    # Send data
-    print >>sys.stderr, 'sending "%s"' % data["id"]
-    sock.sendall(str(data["id"]))
-    return_data = sock.recv(16)
-    print >>sys.stderr, 'received "%s"' % return_data  
-    time.sleep(15)
+    while True:
+        # Send data
+        print >>sys.stderr, 'sending "%s"' % send_alive["id"]
+        sock.sendall(str(send_alive["id"]))
+        return_data = sock.recv(16)
+        if return_data == "8001":
+            print >>sys.stderr, 'received "%s"' % return_data  
+        time.sleep(15)
+
+
+def handshake():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Connect the socket to the port where the server is listening
+    server_address = ('localhost', 10001)
+    print >>sys.stderr, 'connecting to %s port %s\n' % server_address
+    sock.connect(server_address)
+
+    data_handshake = '{"id": 8001}'
+    send_handshake = json.loads(data_handshake)
+
+    while True:
+        # Send data
+        print >>sys.stderr, 'sending "%s"' % send_handshake["id"]
+        sock.sendall(str(send_handshake["id"]))
+        return_data = sock.recv(16)
+        if return_data == "8101":
+            print >>sys.stderr, 'received "%s"' % return_data  
+        time.sleep(15)
+
+
+if __name__ =="__main__":
+    # creating thread
+    t1 = threading.Thread(target=alive_check)
+    t2 = threading.Thread(target=handshake)
+ 
+    # starting thread 1
+    t1.start()
+    # starting thread 2
+    t2.start()
+ 
+    # wait until thread 1 is completely executed
+    t1.join()
+    # wait until thread 2 is completely executed
+    t2.join()
 
--- tcp_client_server.py
+++ tcp_client_server.py
@@ -1,33 +1,83 @@
 import socket
 import sys
 import json
+import threading
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def alive_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
+    server_address = ('localhost', 10000)
+    print >>sys.stderr, 'starting up on %s port %s\n' % server_address
+    sock.bind(server_address)
 
-sock.listen(1)
-return_data = '{"id": 8001}'
-send_data = json.loads(return_data)
+    sock.listen(1)
+    return_alive = '{"id": 8001}'
+    send_alive = json.loads(return_alive)
 
-while True:
-    # Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
+    while True:
+        # Wait for a connection
+        print >>sys.stderr, 'waiting for a connection\n'
+        connection, client_address = sock.accept()
 
-    try:
-        print >>sys.stderr, 'connection from', client_address
+        try:
+            print >>sys.stderr, 'connection from', client_address
 
-        # Receive the data in small chunks and retransmit it
-        while True:
-            data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
-            if data == "8000":
-                print >>sys.stderr, 'sending data back to the client: %s' %send_data["id"]
-                connection.sendall(str(send_data["id"]))
-            
-    finally:
-        # Clean up the connection
-        connection.close()
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(16)
+                print >>sys.stderr, 'received "%s"' % data
+                if data == "8000":
+                    print >>sys.stderr, 'sending data back to the client: %s' %send_alive["id"]
+                    connection.sendall(str(send_alive["id"]))
+                
+        finally:
+            # Clean up the connection
+            connection.close()
+
+def handshake_server():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ('localhost', 10001)
+    print >>sys.stderr, 'starting up on %s port %s' % server_address
+    sock.bind(server_address)
+
+    sock.listen(1)
+    return_handshake = '{"id":8101}'
+    send_handshake = json.loads(return_handshake)
+
+    while True:
+        # Wait for a connection
+        print >>sys.stderr, 'waiting for a connection\n'
+        connection, client_address = sock.accept()
+
+        try:
+            print >>sys.stderr, 'connection from', client_address
+
+            # Receive the data in small chunks and retransmit it
+            while True:
+                data = connection.recv(16)
+                print >>sys.stderr, 'received "%s"' % data
+                if data == "8001":
+                    print >>sys.stderr, 'sending data back to the client: %s' %send_handshake["id"]
+                    connection.sendall(str(send_handshake_handshake["id"]))
+                
+        finally:
+            # Clean up the connection
+            connection.close()
+
+
+
+if __name__ =="__main__":
+    # creating thread
+    t1 = threading.Thread(target=alive_server)
+    t2 = threading.Thread(target=handshake_server)
+ 
+    # starting thread 1
+    t1.start()
+    # starting thread 2
+    t2.start()
+ 
+    # wait until thread 1 is completely executed
+    t1.join()
+    # wait until thread 2 is completely executed
+    t2.join()
